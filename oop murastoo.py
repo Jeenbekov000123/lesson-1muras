cfe454d791abc6527b0a65c876c5944764a9c8ee
Your Name <your.email@example.com>
2024-12-25 14:49:53 +0300

    murasbek

---
+++ oop murastoo.py
@@ -0,0 +1,125 @@
+# class Animals :
+#     def init(self,name,widht,color,country):
+#
+#         self.name = name
+#         self.widht = widht
+#         self.color = color
+#         self.country = country
+#
+#
+#     def golos(self):
+#         print('Animals golos')
+#
+#     def spat (self):
+#         print('Animals spat')
+#
+#     def food (self )  :
+#         print('Animals food')
+#
+#
+# class Dog(Animals):
+#     def __init__(self,name,widht,color,counrty,poroda,pol):
+#         self.name = name
+#         self.widht = widht
+#         self.color = color
+#         self.country = counrty
+#         self.poroda = poroda
+#         self.pol = pol
+#
+#     def fas (self )    :
+#         print(f'{self.name} fas')
+#
+#     def ohrana (self) :
+#         print('protect the house')
+#
+#
+# Animals =Dog ('reks','30','black','kyrgystan','taygan','nam')
+#
+# Animals.fas()
+#
+#
+# class Animals:
+#
+#     def __init__(self, name, widht, color, country):
+#         self.name = name
+#         self.widht = widht
+#         self.color = color
+#         self.country = country
+#
+#     def golos(self):
+#         print('Animals golos')
+#
+#     def spat(self):
+#         print('Animals spat')
+#
+#     def food(self):
+#         print('Animals food')
+#
+#
+# class Cat(Anit__(self,name,widht,color,counrty,poroda,pol):
+#         self.name = name
+#         self.widht = widht
+#         self.color = color
+#         self.country = counrty
+#         self.poroda = poroda
+#         self.pol = pol
+#
+#     def komne(self):
+#         print(f'{self.name} komne')
+#
+#     def spat(self):
+#         print('spat')
+#
+#
+# animals = Cat ('belka','5','white','amreika','британская','nam')
+#
+# animals.spat()nimals):
+#     def __i
+#
+# class Car :
+#      def init(self,Marka,model,color,country):
+#         self.Marka = Marka
+#         self.model = model
+#         self.color = color
+#         self.country = country
+#
+#      def start(self):
+#          print('Car start')
+#
+#      def stop (self):
+#          print('Car stop')
+#
+#      def zavodis (self )  :
+#          print('Car zavodis')
+#
+#
+# class Bmw (Car):
+#      def __init__(self,Marka,model,color,counrty,age,topliva,):
+#          self.Marka = Marka
+#          self.model = model
+#          self.color = color
+#          self.country = counrty
+#          self.age = age
+#          self.topliva = topliva
+#
+#
+#      def name (self )    :
+#          print(f'{self.Marka} start')
+#
+#      def stop (self) :
+#          print('stop')
+#
+#      def mark (self) :
+#          print(f'{self.model} ')
+#
+#      def toplliv (self) :
+#          print(f'{self.topliva} 95')
+#
+# Car =Bmw ('Bmw','f 90','Black','germania','2005','benzin')
+#
+# Car.name()
+# Car.mark()
+# Car.toplliv()
+
+
+
